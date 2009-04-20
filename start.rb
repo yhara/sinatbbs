73a932eed642fc285f23a7c7e04eb29e11ce01b4
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-04-21 04:32:20 +0900

    new: posted date

--- start.rb
+++ start.rb
@@ -6,15 +6,21 @@ Sequel::Model.plugin(:schema)
 DB = Sequel.sqlite("comments.db")
 class Comments < Sequel::Model
   set_schema do
+    primary_key :id
     string :name
     string :title
     text :message
+    timestamp :posted_date
+  end
+
+  def date
+    self.posted_date.strftime("%Y-%m-%d %H:%M:%S")
   end
 end
 Comments.create_table unless Comments.table_exists?
 
 get '/' do
-  @comments = Comments.all
+  @comments = Comments.order_by(:posted_date.desc)
   haml :index
 end
 
@@ -23,6 +29,7 @@ put '/comment' do
     :name => request[:name],
     :title => request[:title],
     :message => request[:message],
+    :posted_date => Time.now,
   })
 
   redirect '/'
