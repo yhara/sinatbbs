9ab8d918e8d3c54223c87e877bac975f56b3208e
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-04-21 03:40:23 +0900

    new: model Comments

--- start.rb
+++ start.rb
@@ -1,6 +1,18 @@
 require 'rubygems'
 require 'sinatra'
+require 'sequel'
+Sequel::Model.plugin(:schema)
+
+DB = Sequel.sqlite("comments.db")
+class Comments < Sequel::Model
+  set_schema do
+    string :name
+    string :title
+    text :message
+  end
+end
+Comments.create_table unless Comments.table_exists?
 
 get '/' do
-  'hello'
+  Comments.all.inspect
 end
