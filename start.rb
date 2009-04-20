7c8e001218d8beb7234f84b72dc81e8a3e9a149e
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-04-21 05:12:01 +0900

    new: stylesheet by sass

--- start.rb
+++ start.rb
@@ -27,6 +27,11 @@ helpers do
   include Rack::Utils; alias_method :h, :escape_html
 end
 
+get '/style.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :style
+end
+
 get '/' do
   @comments = Comments.order_by(:posted_date.desc)
   haml :index
